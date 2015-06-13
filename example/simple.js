8d22cc3f697faac661041459f745f6d3a6a0bfba
juanfmolina <jufermoflo134@hotmail.com>
2015-06-13 11:10:10 -0500

    Commit inicial

---
+++ example/simple.js
@@ -0,0 +1,13 @@
+/*
+ * empresariales-ejemplo-ic
+ * https://github.com/juanfmolina/empresariales-ejemplo-ic
+ *
+ * Copyright (c) 2015 Juan Fernando Molina
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var empresarialesEjemploIc = require('../');
+
+console.log(empresarialesEjemploIc()); // "awesome"
